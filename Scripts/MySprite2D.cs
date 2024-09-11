52aa0d3e5417c3a31b466f8a4bff65dd2ed7d4dd
Summer Gautier <summergautier@gmail.com>
2024-09-11 08:38:34 -0500

    svg can be controlled by user input to rotate and move position

--- Scripts/MySprite2D.cs
+++ Scripts/MySprite2D.cs
@@ -12,14 +12,33 @@ public partial class MySprite2D : Sprite2D
     // functions
     public override void _Process(double delta) // delta is time elapsed since last frame
     {
-        // rotate object orientation in space
-        Rotation += _angularSpeed * (float)delta;
+        // ui_left / ui_right are defaulted to arrow keys and gamepad
+        // this can be modified in the Input Map settings of Godot
+        Int16 direction = 0;
+        if (Input.IsActionPressed("ui_left"))
+        {
+            direction = -1;
+        }
+        if (Input.IsActionPressed("ui_right"))
+        {
+            direction = 1;
+        }
 
-        // move object origin in direction of rotation and at magnitude of speed
-        // Vector2.Up is forward relative to the object, it's modified by the new rotation to 
-        // obtain the new direction.
-        // how much towards that new direction we move is based on the delta fragment
-        Vector2 velocity = Vector2.Up.Rotated(Rotation) * _speed;
+        // rotate object orientation in space
+        Rotation += _angularSpeed * direction * (float)delta;
+
+        // set local velocity dir/magnitude to no movement
+        Vector2 velocity = Vector2.Zero;
+        if (Input.IsActionPressed("ui_up"))
+        {
+            // Vector2.Up is global forward direction, align with this object's forward
+            // direction by rotating the same direction that this object has rotated
+            // velocity is towards that forward direction at the magnitude of _speed;
+            velocity = Vector2.Up.Rotated(Rotation) * _speed;
+        }
+
+        // move towards target dir/mag encapsulated by velocity, but only move
+        // partially to the goal vector. The partial amount is encapsulated by the delta
         Position += velocity * (float)delta;
     }
 
